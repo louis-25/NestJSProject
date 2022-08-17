f8984e7cc3f0d7884a49f593058ec5e9b2b5b00e
louis-25 <20151577@vision.hoseo.edu>
2022-08-18 00:23:50 +0900

    간단한 create read api 추가

--- letsStart/src/app.ts
+++ letsStart/src/app.ts
@@ -3,9 +3,81 @@ import { Cat, CatType } from "./app.model";
 
 const app = express();
 const port = 8000;
+//* logging middleware
+app.use((req, res, next) => {
+  console.log(req.rawHeaders[1]);
+  console.log("this is middleware");
+  next();
+});
 
-app.get("/", (req: express.Request, res: express.Response) => {
-  res.send({ cats: Cat });
+//* json middleware
+app.use(express.json());
+
+//* READ 고양이 전체 데이터 조회
+app.get("/cats", (req, res) => {
+  try {
+    const cats = Cat;
+
+    res.status(200).send({
+      success: true,
+      data: {
+        cats,
+      },
+    });
+  } catch (error: any) {
+    res.status(400).send({
+      success: false,
+      error: error.message,
+    });
+  }
+  const cats = Cat;
+});
+
+//* READ 특정 고양이 데이터 조회
+app.get("/cats/:id", (req, res) => {
+  try {
+    const params = req.params;
+    console.log("params", params);
+    const cats = Cat.find((cat) => {
+      return cat.id === params.id;
+    });
+
+    res.status(200).send({
+      success: true,
+      data: {
+        cats,
+      },
+    });
+  } catch (error: any) {
+    res.status(400).send({
+      success: false,
+      error: error.message,
+    });
+  }
+});
+
+//* CREATE 새로운 고양이 추가 api
+app.post("/cats", (req, res) => {
+  try {
+    const data = req.body;
+    console.log("data", data);
+    Cat.push(data);
+    res.status(200).send({
+      success: true,
+      data: { data },
+    });
+  } catch (error: any) {
+    res.status(400).send({
+      success: false,
+      error: error.message,
+    });
+  }
+});
+
+//* 404 middleware
+app.use((req, res, next) => {
+  console.log("this is error middleware");
+  res.send({ error: "404 not found error" });
 });
 
 app.listen(port, () => {
