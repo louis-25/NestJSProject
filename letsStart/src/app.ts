ec8076c7a97bc83b5dac773173706973072f3f10
louis-25 <20151577@vision.hoseo.edu>
2022-08-17 22:12:56 +0900

    데이터 모킹

--- letsStart/src/app.ts
+++ letsStart/src/app.ts
@@ -1,10 +1,11 @@
 import express from "express";
+import { Cat, CatType } from "./app.model";
 
 const app = express();
 const port = 8000;
 
 app.get("/", (req: express.Request, res: express.Response) => {
-  res.send("Hello World!");
+  res.send({ cats: Cat });
 });
 
 app.listen(port, () => {
